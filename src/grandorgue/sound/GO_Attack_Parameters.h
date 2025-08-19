167213dd29e83c230658e334b1bc9186f32ce8df
Sebion7125 <sebion7125@gmail.com>
2025-08-19 10:02:40 +0200

    Sinus equal Power crossfade added. New Linear Model for Attack-Release Gain Control integrated. Reading Hardcoded example data from file GO_Attack_Parameters.cpp.

--- src/grandorgue/sound/GO_Attack_Parameters.h
+++ src/grandorgue/sound/GO_Attack_Parameters.h
@@ -2,7 +2,7 @@
 
 // Lookup-Tabellen für Einschwingverhalten pro MIDI-Note (C0 bis G9)
 
-// Dry
+/*// Dry altes modell
 extern const float curvature_dry_by_midi[128];     // Parabelfit-Krümmung
 extern const float attack_time_dry_by_midi[128];   // Attack-Zeit in Millisekunden
 
@@ -12,4 +12,18 @@ extern const float attack_time_front_by_midi[128]; // Attack-Zeit in Millisekund
 
 // Rear
 extern const float curvature_rear_by_midi[128];    // Parabelfit-Krümmung
-extern const float attack_time_rear_by_midi[128];  // Attack-Zeit in Millisekunden
+extern const float attack_time_rear_by_midi[128];  // Attack-Zeit in Millisekunden*/
+
+
+
+// Dry
+extern const float g0_dry_by_midi[128];
+extern const float tmax_dry_by_midi[128];
+
+// Front
+extern const float g0_front_by_midi[128];
+extern const float tmax_front_by_midi[128];
+
+// Rear
+extern const float g0_rear_by_midi[128];
+extern const float tmax_rear_by_midi[128];
