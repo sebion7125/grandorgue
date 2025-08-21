f1f1b2b79198b4f48aec4c526356c228fb4cc89a
Sebion7125 <sebion7125@gmail.com>
2025-08-21 12:24:27 +0200

    Stop tracking local IDE/cache files: .ccls-cache, .cache/clangd, .vscode, o.ccls, .clangd

--- .ccls-cache/@home@vboxuser@grandorgue/src@grandorgue@sound@GO_Attack_Parameters.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// Lookup-Tabellen für Einschwingverhalten pro MIDI-Note (C0 bis G9)
-
-extern const float curvature_by_midi[128];     // Parabelfit-Krümmung
-extern const float attack_time_by_midi[128];   // Attack-Zeit in Millisekunden
