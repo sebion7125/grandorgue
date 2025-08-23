c1af07f88fd3f36b50b996e017f708216416a396
Sebion7125 <sebion7125@gmail.com>
2025-08-23 23:47:52 +0200

    GOProgressDialog: increase padding to improve dialog width on many platforms

--- src/grandorgue/gui/dialogs/GOProgressDialog.cpp
+++ src/grandorgue/gui/dialogs/GOProgressDialog.cpp
@@ -45,7 +45,7 @@ void GOProgressDialog::Setup(
   // Some wx ports do not expose SetSize/GetSize on wxProgressDialog; adding
   // trailing spaces to the initial message helps the dialog lay out wider.
   wxString paddedMsg = msg;
-  const int padSpaces = 40; // approx +10% width on typical systems
+  const int padSpaces = 120; // larger padding to increase visible width
   paddedMsg += wxString(padSpaces, ' ');
 
   m_dlg = new wxProgressDialog(
